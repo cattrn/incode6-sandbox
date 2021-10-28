5277f8c00bec8f7cc402f8505a4efbaac8ca52d3
karatkuna <karat.kuna@gmail.com>
2021-10-28 13:12:43 +1100

    Create database connection

---
+++ database.js
@@ -0,0 +1,15 @@
+const pgp = require('pg-promise')()
+
+const user = process.env.DB_USER
+const password = process.env.DB_PASSWORD
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT
+const database = process.env.DB_DATABASE
+
+// CONNECTION STRING
+const connection = `postgres://${user}:${password}@${host}:{port}/{database}`
+
+const db = pgp(connection)
+
+module.exports = db
+
